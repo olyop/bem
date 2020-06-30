7953c9313b6673f452c3c7033626bf430cf4b727
Oliver <oliver.plummer@outlook.com>
2020-06-30 17:28:49 +1000

    Update

--- index.ts
+++ index.ts
@@ -1,80 +1,56 @@
-type UnknownFunction = (...params: unknown[]) => unknown
+import { pipe } from "@oly_op/pipe"
+import { isNull, isEmpty, isString, isUndefined } from "lodash"
 
-export function pipe<A extends unknown[], B>(
-	ab: (this: void, ...a: A) => B,
-): (...args: A) => B
-
-export function pipe<A extends unknown[], B, C>(
-	ab: (this: void, ...a: A) => B,
-	bc: (this: void, b: B) => C,
-): (...args: A) => C
-
-export function pipe<A extends unknown[], B, C, D>(
-	ab: (this: void, ...a: A) => B,
-	bc: (this: void, b: B) => C,
-	cd: (this: void, c: C) => D,
-): (...args: A) => D
-
-export function pipe<A extends unknown[], B, C, D, E>(
-	ab: (this: void, ...a: A) => B,
-	bc: (this: void, b: B) => C,
-	cd: (this: void, c: C) => D,
-	de: (this: void, d: D) => E,
-): (...args: A) => E
-
-export function pipe<A extends unknown[], B, C, D, E, F>(
-	ab: (this: void, ...a: A) => B,
-	bc: (this: void, b: B) => C,
-	cd: (this: void, c: C) => D,
-	de: (this: void, d: D) => E,
-	ef: (this: void, e: E) => F,
-): (...args: A) => F
-export function pipe<A extends unknown[], B, C, D, E, F, G>(
-	ab: (this: void, ...a: A) => B,
-	bc: (this: void, b: B) => C,
-	cd: (this: void, c: C) => D,
-	de: (this: void, d: D) => E,
-	ef: (this: void, e: E) => F,
-	fg: (this: void, f: F) => G,
-): (...args: A) => G
-
-export function pipe<A extends unknown[], B, C, D, E, F, G, H>(
-	ab: (this: void, ...a: A) => B,
-	bc: (this: void, b: B) => C,
-	cd: (this: void, c: C) => D,
-	de: (this: void, d: D) => E,
-	ef: (this: void, e: E) => F,
-	fg: (this: void, f: F) => G,
-	gh: (this: void, g: G) => H,
-): (...args: A) => H
-
-export function pipe<A extends unknown[], B, C, D, E, F, G, H, I>(
-	ab: (this: void, ...a: A) => B,
-	bc: (this: void, b: B) => C,
-	cd: (this: void, c: C) => D,
-	de: (this: void, d: D) => E,
-	ef: (this: void, e: E) => F,
-	fg: (this: void, f: F) => G,
-	gh: (this: void, g: G) => H,
-	hi: (this: void, h: H) => I,
-): (...args: A) => I
-
-export function pipe<A extends unknown[], B, C, D, E, F, G, H, I, J>(
-	ab: (this: void, ...a: A) => B,
-	bc: (this: void, b: B) => C,
-	cd: (this: void, c: C) => D,
-	de: (this: void, d: D) => E,
-	ef: (this: void, e: E) => F,
-	fg: (this: void, f: F) => G,
-	gh: (this: void, g: G) => H,
-	hi: (this: void, h: H) => I,
-	ij: (this: void, i: I) => J,
-): (...args: A) => J
-
-export function pipe(...fns: UnknownFunction[]): UnknownFunction {
-	return (...initialParams: unknown[]): unknown =>
-		fns.reduce<unknown>((value, fn, index) => {
-			const params = index === 0 ? (value as unknown[]) : [value]
-			return fn(...params)
-		}, initialParams)
+export interface ClassType {
+	ignore: boolean,
+	className: string,
 }
+
+export type BemInputType =
+	ClassType | string | null | undefined
+
+const isUpperCase = (x: string): boolean =>
+	x === x.toUpperCase()
+
+const createClassType = (className: string, ignore = false): ClassType =>
+	({ ignore, className })
+
+const normalizeInput = (classNames: BemInputType[]): ClassType[] =>
+	classNames
+		.map((className) => {
+			if (isNull(className) || isUndefined(className)) {
+				return createClassType("", true)
+			} else if (isString(className)) {
+				if (isEmpty(className)) {
+					return createClassType(className)
+				} else if (isUpperCase(className.charAt(0))) {
+					return createClassType(className, true)
+				} else {
+					return createClassType(className)
+				}
+			} else {
+				return className
+			}
+		})
+		.filter((className) => className !== null)
+
+const mapBemValues = (componentName: string) => (classNames: ClassType[]) =>
+	classNames.map(
+		({ ignore, className }) => {
+			if (ignore) {
+				return className
+			} else if (isEmpty(className)) {
+				return componentName
+			} else {
+				return `${componentName}__${className}`
+			}
+		},
+	)
+
+const joinToString = (classNames: string[]) =>
+	classNames.join(" ")
+
+export const createBem =
+	(componentName: string) =>
+		(...classNames: BemInputType[]): string =>
+			pipe(normalizeInput, mapBemValues(componentName), joinToString)(classNames)
