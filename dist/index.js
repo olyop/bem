90a7bc4e2c7cbfb4d9cb46a9f2ad31e9386ac7a9
Oliver <oliver.plummer@outlook.com>
2020-07-08 17:41:54 +1000

    Update

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,14 @@
-import isNull from "lodash/isNull";
-import { pipe } from "@oly_op/pipe";
-import isEmpty from "lodash/isEmpty";
-import isString from "lodash/isString";
-import isUndefined from "lodash/isUndefined";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createBem = void 0;
+const pipe_1 = require("@oly_op/pipe");
+const isEmpty_1 = __importDefault(require("lodash/isEmpty"));
+const isNull = (val) => val === null;
+const isString = (val) => typeof val === "string";
+const isUndefined = (val) => val === undefined;
 const isUpperCase = (x) => x === x.toUpperCase();
 const createClassType = (className, ignore = false) => ({ ignore, className });
 const normalizeInput = (classNames) => classNames
@@ -11,7 +17,7 @@ const normalizeInput = (classNames) => classNames
         return createClassType("", true);
     }
     else if (isString(className)) {
-        if (isEmpty(className)) {
+        if (isEmpty_1.default(className)) {
             return createClassType(className);
         }
         else if (isUpperCase(className.charAt(0))) {
@@ -30,7 +36,7 @@ const mapBemValues = (componentName) => (classNames) => classNames.map(({ ignore
     if (ignore) {
         return className;
     }
-    else if (isEmpty(className)) {
+    else if (isEmpty_1.default(className)) {
         return componentName;
     }
     else {
@@ -38,4 +44,4 @@ const mapBemValues = (componentName) => (classNames) => classNames.map(({ ignore
     }
 });
 const joinToString = (classNames) => classNames.join(" ");
-export const createBem = (componentName) => (...classNames) => pipe(normalizeInput, mapBemValues(componentName), joinToString)(classNames);
+exports.createBem = (componentName) => (...classNames) => pipe_1.pipe(normalizeInput, mapBemValues(componentName), joinToString)(classNames);
